1878d24721d4797e074152cccbdabb975e9c867f
RogerDavidCruz <51060530+RogerDavidCruz@users.noreply.github.com>
2019-07-02 21:46:51 -0400

    delete main.js file, updated new file

--- codewars/liSetAlarm/main.js
+++
@@ -1,6 +0,0 @@
-function setAlarm(employed, vacation){
- if (employed==true && vacation== true ) { return false }
- if (employed==false && vacation== true ) { return false }
-if (employed==false && vacation== false) { return false }
-if (employed==true && vacation== false ) { return true }
-}
