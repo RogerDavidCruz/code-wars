cc9309c7b402973d9dcc03a1a0f172dc7b4a5265
RogerDavidCruz <51060530+RogerDavidCruz@users.noreply.github.com>
2019-07-05 23:09:57 -0400

    week 7 - exercise 5

---
+++ opposite-number/main.js
@@ -0,0 +1,16 @@
+//codewar coding challenge
+//Opposite Number
+
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+//
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+//solution
+function opposite(number) {
+  return(-number)
+}
