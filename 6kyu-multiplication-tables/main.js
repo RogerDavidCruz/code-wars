acdbde39bd6f689b279540c41ed117d5bbc0eb40
RogerDavidCruz <51060530+RogerDavidCruz@users.noreply.github.com>
2019-07-13 22:23:13 -0400

    week 8 - exercise 2

---
+++ 6kyu-multiplication-tables/main.js
@@ -0,0 +1,28 @@
+//Coding Challenge
+//6kyu - Multiplication Tables
+
+// Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
+//
+// Example:
+//
+// multiplication_table(3,3)
+//
+// 1 2 3
+// 2 4 6
+// 3 6 9
+//
+// -->[[1,2,3],[2,4,6],[3,6,9]]
+//
+// Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column.
+
+//Solution
+function multiplicationTable(row, col) {
+  let multiArray = [];
+  for (let i = 0; i < row; i++) {
+    multiArray.push([]);
+    for (let j = 0; j < col; j++) {
+      multiArray[i].push((i+1) * (j+1));
+    }
+  }
+  return multiArray;
+}
