bdd3fa5334d4c167a780289e9eebab31fd489b5c
Roger David Cruz <roger.david.cruz@gmail.com>
2020-04-15 16:00:26 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_grasshopper_function_syntax_debugging.js
@@ -0,0 +1,14 @@
+// 8 kyu - Grasshopper - Function syntax debugging
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/javascript
+
+// A student was working on a function and made some syntax mistakes while 
+// coding. Help them find their mistakes and fix them.
+
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+// Solution
+function main (verb, noun){
+  return verb + noun;
+}
