5b12771e42ff21950ea7dd168c382302e5e86dfd
Roger David Cruz <roger.david.cruz@gmail.com>
2020-04-18 00:06:18 -0400

    codewar challenge

--- #_The_Post_RC_Saga_Training/SQL_practice/7kyu_codewars/7kyu_SQL_basic_simple.js
+++ #_The_Post_RC_Saga_Training/SQL_practice/7kyu_codewars/7kyu_SQL_basic_simple.js
@@ -1,3 +1,4 @@
+// SQL Basics: Simple JOIN with COUNT
 // https://www.codewars.com/kata/580918e24a85b05ad000010c/train/sql
 
 // For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys
@@ -11,7 +12,8 @@
 // people_id
 // You should return all people fields as well as the toy count as "toy_count".
 
-SELECT p.id, p.name, COUNT(toys) AS toy_count
+SELECT p.*, COUNT(t) AS toy_count
 FROM people p
-INNER JOIN toys t
-ON p.id = t.people_id
+JOIN toys t
+  ON t.people_id = p.id
+GROUP BY p.id
