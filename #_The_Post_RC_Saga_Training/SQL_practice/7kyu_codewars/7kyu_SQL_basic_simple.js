a9b8a37400de843efb2db4605425ae69b4ace707
Roger David Cruz <roger.david.cruz@gmail.com>
2020-04-17 23:59:34 -0400

    codewar

---
+++ #_The_Post_RC_Saga_Training/SQL_practice/7kyu_codewars/7kyu_SQL_basic_simple.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/580918e24a85b05ad000010c/train/sql
+
+// For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys
+//
+// people table schema
+// id
+// name
+// toys table schema
+// id
+// name
+// people_id
+// You should return all people fields as well as the toy count as "toy_count".
+
+SELECT p.id, p.name, COUNT(toys) AS toy_count
+FROM people p
+INNER JOIN toys t
+ON p.id = t.people_id
