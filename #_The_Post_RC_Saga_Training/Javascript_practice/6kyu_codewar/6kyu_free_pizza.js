e6b225924ed8edcc41271b876de75b7d6c2fa58b
Roger David Cruz <roger.david.cruz@gmail.com>
2020-04-19 23:42:18 -0400

    codewar challege

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/6kyu_codewar/6kyu_free_pizza.js
@@ -0,0 +1,22 @@
+// 6 kyu - Free pizza
+// https://www.codewars.com/kata/595910299197d929a10005ae/train/javascript
+
+// In an attempt to boost sales, the manager of the pizzeria you work at has devised a pizza rewards system: if you already made at least 5 orders of at least 20 dollars, you get a free 12 inch pizza with 3 toppings of your choice.
+//
+// However, the rewards system may change in the future. Your manager wants you to implement a function that, given a dictionary of customers, a minimum number of orders and a minimum order value, returns a set of the customers who are eligible for a reward.
+//
+// Customers in the dictionary are represented as:
+//
+// { 'customerName' : [list_of_order_values_as_integers] }
+// See example test case for more details.
+
+function pizzaRewards(customers, minOrders, minPrice) {
+    let positiveCustomers = [];
+
+    for (let pizza in customers) {
+        if (customers[pizza].filter(e => e >= minPrice).length >= minOrders) {
+            positiveCustomers.push(pizza);
+        }
+    }
+    return positiveCustomers;
+}
