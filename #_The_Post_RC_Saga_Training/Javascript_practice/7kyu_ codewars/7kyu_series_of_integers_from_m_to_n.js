0650e4fab50a6d103101b36d28ad1db78275da70
Roger David Cruz <roger.david.cruz@gmail.com>
2020-04-27 23:42:21 -0400

    codewars challenge

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_series_of_integers_from_m_to_n.js
@@ -0,0 +1,19 @@
+// 7 kyu - Series of integers from m to n
+// https://www.codewars.com/kata/5841f680c5c9b092950001ae/train/javascript
+
+// Task
+// Write a function that accepts two arguments and generates a sequence containing the integers from the first argument to the second inclusive.
+//
+// Input
+// Pair of integers greater than or equal to 0. The second argument will always be greater than or equal to the first.
+//
+// Example
+// generateIntegers(2, 5) // --> [2, 3, 4, 5]
+
+function generateIntegers(m, n) {
+  let intSeries=[];
+  for (let i=m; i<=n; i++){
+    intSeries.push(i)
+  }
+  return intSeries;
+}
