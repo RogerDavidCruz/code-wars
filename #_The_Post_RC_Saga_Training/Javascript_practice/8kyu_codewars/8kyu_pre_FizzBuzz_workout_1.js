32e63564a5459c19707815b9f40bef6d9f81208a
Roger David Cruz <roger.david.cruz@gmail.com>
2020-05-26 10:33:47 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_pre_FizzBuzz_workout_1.js
@@ -0,0 +1,10 @@
+// 8 kyu - Pre-FizzBuzz Workout #1
+// https://www.codewars.com/kata/569e09850a8e371ab200000b/train/javascript
+
+function preFizz(n) {
+  let list = [];
+  for(let i = 1; i<=n; i++){
+    list.push(i);
+  }
+  return list;
+}
