dad70111fc95c101fa3f2eef51c6b942d6e58201
Roger David Cruz <roger.david.cruz@gmail.com>
2020-05-30 22:54:47 -0400

    codewars challenge

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_grasshopper_messi_goals.js
@@ -0,0 +1,26 @@
+// 8 kyu - Grasshopper - Messi Goals
+// https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/javascript
+//
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+//
+// Information
+// Messi goal scoring statistics:
+//
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Define
+// Create three variables and store the appropriate values using the table above:
+//
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals (javascript, C#, Java) or total_goals (python, ruby). totalGoals/total_goals should store the sum of all of Messi's goals for this year.
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = (laLigaGoals + championsLeagueGoals + copaDelReyGoals)
