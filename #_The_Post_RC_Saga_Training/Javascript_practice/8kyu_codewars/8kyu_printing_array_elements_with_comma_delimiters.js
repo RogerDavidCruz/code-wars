dc831729dc28435cd969dd4bd3136bd6eccf4df9
Roger David Cruz <roger.david.cruz@gmail.com>
2020-06-16 12:50:39 -0400

    printing_array_elements_with_comma_delimiters

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_printing_array_elements_with_comma_delimiters.js
@@ -0,0 +1,11 @@
+// 8 kyu - Printing Array elements with Comma delimiters
+// https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+
+const printArray = array => array.join(',');
+
+//prep:
+
+//p: an array
+//r: a string with commans between each element
+//e: [2,4,5,2] => "2,4,5,2"
+//p: use the join method and pass the string containing a comma
