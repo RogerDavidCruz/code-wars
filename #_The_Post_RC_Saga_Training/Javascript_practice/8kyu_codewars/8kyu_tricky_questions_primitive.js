9acec86eac214c606cbfd8b8a13f6a08fdded44d
Roger David Cruz <roger.david.cruz@gmail.com>
2020-07-18 13:19:07 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_tricky_questions_primitive.js
@@ -0,0 +1,8 @@
+// 8kyu - tricky questions primitive
+// https://www.codewars.com/kata/572269697297444c570008b9/train/javascript
+
+function greaterThanLessThan (a,b,c) {
+  //initial commit testing
+  }
+
+  
