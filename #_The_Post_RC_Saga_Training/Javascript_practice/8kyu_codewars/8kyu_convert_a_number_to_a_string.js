536cadc2efb454a4700e7b85a3b9ef5c4b23a11c
Roger David Cruz <roger.david.cruz@gmail.com>
2020-07-28 16:24:03 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_convert_a_number_to_a_string.js
@@ -0,0 +1,8 @@
+// 8 kyu - Convert a Number to a String!
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+//
+// We need a function that can transform a number into a string.
+
+function numberToString(num) {
+  return num.toString()
+}
