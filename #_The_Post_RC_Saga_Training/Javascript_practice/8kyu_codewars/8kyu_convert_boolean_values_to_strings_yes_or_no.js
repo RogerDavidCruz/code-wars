cd98451fbade72e74f3d2701de4d5d5db666dd2b
Roger David Cruz <roger.david.cruz@gmail.com>
2020-07-31 16:18:50 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_convert_boolean_values_to_strings_yes_or_no.js
@@ -0,0 +1,9 @@
+// 8 kyu - Convert boolean values to strings 'Yes' or 'No'.
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+//
+// Complete the method that takes a boolean value and return a "Yes" string
+// for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  return bool === true ? "Yes" : "No";
+}
