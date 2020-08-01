258a0a6abadb6940346e2bbe9d4f96460c65ce24
Roger David Cruz <roger.david.cruz@gmail.com>
2020-08-01 15:18:28 -0400

    codewar practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_simple_string_division.js
@@ -0,0 +1,17 @@
+// 7 kyu - Simple string division
+// https://www.codewars.com/kata/5b83c1c44a6acac33400009a/train/javascript
+//
+// In this Kata, you will be given a number in form of a string and an integer
+// k and your task is to insert k commas into the string and determine which of
+// the partitions is the largest.
+//
+// For example:
+// solve('123',1) = 23 because we insert one comma and get the substrings ('1','23') or ('12',3). The max of these is '23'.
+// solve('1234',1) = 234 because ('1','234') or ('12','34') or ('123','4').
+// solve('1234',2) = 34 because ('1','2','34') or ('1','23','4') or ('12','3','4').
+// solve('1234',3) = 4.
+
+function solve(str,k){
+  let result = parseInt(str.split(k)[1])
+  return result
+}
