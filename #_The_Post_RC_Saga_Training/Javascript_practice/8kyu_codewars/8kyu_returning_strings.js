e50ab68d49b1593999f1798d2b4ab623e2bf2745
Roger David Cruz <roger.david.cruz@gmail.com>
2020-08-08 23:19:32 -0700

    codewars practice

--- #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_returning_strings.js
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_returning_strings.js
@@ -1,6 +1,7 @@
 // 8 kyu - Returning Strings
 // https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
 //
+//
 // Make a function that will return a greeting statement that uses an input;
 // your program should return, "Hello, <name> how are you doing today?".
 //
