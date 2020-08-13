deb57f57fc5026b08ec2f3d7573fb95f52f46dd7
Roger David Cruz <roger.david.cruz@gmail.com>
2020-08-13 15:21:03 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_return_negative.js
@@ -0,0 +1,9 @@
+// 8 kyu - Return Negative
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+//
+// In this simple assignment you are given a number and have to make it negative.
+// But maybe the number is already negative?
+
+function makeNegative(num) {
+  return num % 2 === 0 ? num*-1 : num;
+}
