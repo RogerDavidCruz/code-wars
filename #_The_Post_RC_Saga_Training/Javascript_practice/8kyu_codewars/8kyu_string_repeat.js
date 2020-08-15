3b1e178b099137426d4ab35904f740f8b71686c8
Roger David Cruz <roger.david.cruz@gmail.com>
2020-08-15 17:39:42 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_string_repeat.js
@@ -0,0 +1,9 @@
+// 8 kyu - String repeat
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+//
+// Write a function called repeatString which repeats the given
+// string src exactly count times.
+
+function repeatStr (n, s) {
+  return s.repeat(n)
+}
