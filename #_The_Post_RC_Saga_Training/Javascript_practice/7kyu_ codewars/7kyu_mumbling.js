2844bed44a55be37c8a8fceb1e9111460fcc1d6a
Roger David Cruz <roger.david.cruz@gmail.com>
2020-08-27 12:28:50 -0400

    codewars practice

--- #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_mumbling.js
+++ #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_mumbling.js
@@ -4,6 +4,13 @@
 // This time no story, no theory. The examples below show you how to
 // write function accum:
 
+// Examples:
+//
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
 function accum(s) {
   return s.split('').map((currentValue, index) => (currentValue.toUpperCase() + currentValue.toLowerCase().repeat(index))).join('-');
 }
---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_shortest_word.js
@@ -0,0 +1,11 @@
+// 7 kyu - Shortest Word
+// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
+
+// Simple, given a string of words, return the length of the shortest word(s).
+//
+// String will never be empty and you do not need to account for different
+// data types.
+
+function findShort(s){
+  return s.split(' ').map(x => x.length).sort((a,b) => a-b).shift()
+}
