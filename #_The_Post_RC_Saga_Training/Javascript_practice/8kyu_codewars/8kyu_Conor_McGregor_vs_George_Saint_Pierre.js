f75a61bbbd7e44a76ce592de7c263a56827e7878
Roger David Cruz <roger.david.cruz@gmail.com>
2020-09-02 10:14:35 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_Conor_McGregor_vs_George_Saint_Pierre.js
@@ -0,0 +1,11 @@
+// 8 kyu - For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+// https://www.codewars.com/kata/582dafb611d576b745000b74/train/javascript
+
+var quote = function(fighter) {
+  if(fighter.toLowerCase() === 'george saint pierre'){
+    return "I am not impressed by your performance.";
+  }
+  else if(fighter.toLowerCase() === 'conor mcgregor'){
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  }
+};
