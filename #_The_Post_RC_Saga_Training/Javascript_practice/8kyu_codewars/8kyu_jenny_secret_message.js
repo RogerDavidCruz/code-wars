f06e2b03cfea72fc85594b1fb0cf42137c87a1d0
Roger David Cruz <roger.david.cruz@gmail.com>
2020-09-09 19:47:24 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_jenny_secret_message.js
@@ -0,0 +1,7 @@
+// 8 kyu - Jenny's secret message
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/javascript
+
+function greet(name){
+  if(name === "Johnny") return "Hello, my love!";
+  return "Hello, " + name + "!";
+}
