45127d37c2cc2b2173f68724cf033f7bb9bb33ab
Roger David Cruz <roger.david.cruz@gmail.com>
2020-09-12 10:05:36 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/6kyu_codewar/6kyu_who_likes_it.js
@@ -0,0 +1,20 @@
+// 6 kyu - Who likes it?
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+
+function likes(names) {
+  if(names.length === 0){
+    return "no one likes this";
+  } 
+  else if(names.length === 1){
+    return `${names} likes this`;
+  }
+  else if(names.length === 2){
+    return `${names[0]} and ${names[1]} like this`;
+  }
+  else if(names.length === 3){
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  }
+  else{
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
