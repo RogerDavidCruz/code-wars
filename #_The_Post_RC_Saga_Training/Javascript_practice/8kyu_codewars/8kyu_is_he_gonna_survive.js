931f4536ccc699549a0f8c4113c11dc52e844369
Roger David Cruz <roger.david.cruz@gmail.com>
2020-10-28 19:54:56 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_is_he_gonna_survive.js
@@ -0,0 +1,6 @@
+// 8 kyu - Is he gonna survive?
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons){
+  return bullets >= 2*dragons ? true : false;
+}
