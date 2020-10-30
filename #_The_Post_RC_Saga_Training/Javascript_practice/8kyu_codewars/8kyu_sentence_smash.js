82708c063f6c8cedd8cdabaa85edaad30d46868e
Roger David Cruz <roger.david.cruz@gmail.com>
2020-10-30 16:21:12 -0400

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_sentence_smash.js
@@ -0,0 +1,11 @@
+// 8kyu - Sentence Smash
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+function smash (words) {
+  return words.join(' ');
+};
+
+//prep
+
+//join the array of words with spacing between words
+//use join js method
