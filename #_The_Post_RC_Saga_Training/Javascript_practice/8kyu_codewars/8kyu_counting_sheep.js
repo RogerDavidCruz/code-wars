35d6fd78ae3c0cf6a007b65b11c85b0abef1099f
Roger David Cruz <roger.david.cruz@gmail.com>
2020-11-18 13:54:53 -0500

    codewars practice

--- #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_counting_sheep.js
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_counting_sheep.js
@@ -1,10 +1,7 @@
 // 8 kyu - Counting sheep...
 // https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
-//
-// Consider an array/list of sheep where some sheep may be missing from their
-// place. We need a function that counts the number of sheep present in the
-// array (true means present).
 
+//Solution 1
 function countSheeps(arrayOfSheep) {
   let count = 0;
   for(let i = 0; i<arrayOfSheep.length; i++){
@@ -16,3 +13,34 @@ function countSheeps(arrayOfSheep) {
   }
   return count;
 }
+
+// prep description
+
+// Consider an array/list of sheep where some sheep may be missing from their
+// place. We need a function that counts the number of sheep present in the
+// array (true means present).
+
+// ------------------------------------------------------------------------------------------
+
+//Solution 2
+
+const countSheeps = arrayOfSheeps => arrayOfSheeps.filter(Boolean).length
+
+//prep
+
+//parameters --------> arrayOfSheeps is an array containing true and false values
+//returns -----------> the number of times true exists in the parameter array
+//example -----------> param: [true,  true,  true,  false,
+//                            true,  true,  true,  true ,
+//                            true,  false, true,  false,
+//                            true,  false, false, true ,
+//                            true,  true,  true,  true ,
+//                            false, false, true,  true ]
+//                     return: 17
+//pseudo code -------> using ES6 syntax, we use the filter array method in js where
+//                     we filter out all the false values using the Boolean object,
+//                     the Boolean object has an initial value of false and therefore
+//                     can use it inside the filter method to return an array with only
+//                     true values, then we take the length of this new array to have
+//                     the number of true values and return that number, while using 
+//                     implicit ES6 syntax 
