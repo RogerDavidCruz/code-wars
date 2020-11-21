0cb3f7bf3035404ddd09ed8c5f15f89386b72131
Roger David Cruz <roger.david.cruz@gmail.com>
2020-11-21 11:15:07 -0500

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_convert_a_string_to_an_array.js
@@ -0,0 +1,13 @@
+// 8 kyu - Convert a string to an array
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+
+function stringToArray(string){
+  return string.split(' ');
+}
+
+//prep
+
+//parameter --------> a string
+//return -----------> an array of strings (words)
+//example ----------> param: "Robin Singh", return: ["Robin", "Singh"]
+//pseudo code ------> use the split method with an empty string with space argument
