a1299018a65957ccb4e9e59264fbb8efc7b44204
Roger David Cruz <roger.david.cruz@gmail.com>
2020-11-25 11:17:52 -0500

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_third_angle_of_a_triangle.js
@@ -0,0 +1,13 @@
+// 8 kyu - Third Angle of a Triangle
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+function otherAngle(a, b) {
+  return 180-a-b;
+}
+
+//prep
+
+//parameters -----> two numbers
+//return ---------> a number 
+//examples -------> param: 30, 60 return: 90
+//pseudo code ----> subtract a and b from 180 and return that number
