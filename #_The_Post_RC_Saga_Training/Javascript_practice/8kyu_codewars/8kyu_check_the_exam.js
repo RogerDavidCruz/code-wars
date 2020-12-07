4a91280d17be3211b06d1bf0fa8f7754cd39a076
Roger David Cruz <roger.david.cruz@gmail.com>
2020-12-07 16:18:27 -0500

    codewars practice

--- #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_check_the_exam.js
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_check_the_exam.js
@@ -1,6 +1,8 @@
 // 8 kyu - Check the exam
 // https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/javascript
 
+//Solution 1
+
 const checkExam = (array1, array2) => {
   let finalScores = [];
   array2.map((response, index) => response === "" ? finalScores.push(0) : response === array1[index] ? finalScores.push(4) : finalScores.push(-1));
@@ -19,3 +21,17 @@ const checkExam = (array1, array2) => {
  //                   tools: includes, map, indexOf, reduce, and ternary 
  //time: >20 min
  
+
+ //Solution 2
+
+const checkExam = (array1, array2) => {
+  const total = array2.reduce((acc, cv, idx) => cv === '' ? acc : cv === array1[idx] ? acc += 4 : --acc, 0);
+  return total <= 0 ? 0 : total;
+ }
+ 
+ //Approach 2 Notes:
+ // Use all three parameters of the reducer function that is passed
+ // as a call back within the reduce method, and initialize the 
+ // starting value for the reduce method to begin at. Then return 
+ // the total score as zero if the summed value is negative.
+ // Time: ~10min
