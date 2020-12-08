e31e343a48e4341750c40e8e758614ad268eb922
Roger David Cruz <roger.david.cruz@gmail.com>
2020-12-08 13:21:33 -0500

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_no_loops_2_you_only_need_one.js
@@ -0,0 +1,14 @@
+// 8kyu - No Loops 2 - You only need one
+// https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce/train/javascript
+
+const check = (a,x) => a.includes(x);
+
+//prep
+
+//parameter: an array (a) and value(number/string) (x)
+//return: true or false (if the array contains the number)
+//example: param: [66, 101], 66 return: true
+//         param: ['what', 'a', 'great', 'kata'], 'kat' return: false
+//pseudo code: use the includes method on the array and pass the value x 
+//             as a param for the includes method
+//time: ~5min 
