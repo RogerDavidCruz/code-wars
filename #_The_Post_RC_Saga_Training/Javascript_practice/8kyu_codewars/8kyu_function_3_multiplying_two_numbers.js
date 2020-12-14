417bb621591f6318a4c76f94ab17d9423f15502a
Roger David Cruz <roger.david.cruz@gmail.com>
2020-12-14 18:29:10 -0500

    codewars practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_function_3_multiplying_two_numbers.js
@@ -0,0 +1,14 @@
+// 8 kyu - Function 3 - multiplying two numbers
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+// Write here your multiply-function
+
+const multiply = (num1, num2) => num1 * num2;
+
+//prep
+
+//p: 2 numbers
+//r: number
+//e: p: 2, 3 r: 6
+//p: create a function using ES6 big arrow syntaxt that takes
+//   in two parameters and returhns a new number after multiplying them.
