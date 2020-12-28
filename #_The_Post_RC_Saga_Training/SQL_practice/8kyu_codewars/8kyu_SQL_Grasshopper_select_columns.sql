e4123d661473514a388bd5886a89a82594b3e9d0
Roger David Cruz <roger.david.cruz@gmail.com>
2020-12-28 12:46:37 -0500

    8kyu_SQL_Grasshopper_select_columns

---
+++ #_The_Post_RC_Saga_Training/SQL_practice/8kyu_codewars/8kyu_SQL_Grasshopper_select_columns.sql
@@ -0,0 +1,12 @@
+-- 8 kyu - SQL Grasshopper: Select Columns
+-- https://www.codewars.com/kata/582365c18917435ab3000020/train/sql
+
+SELECT custid, custname, custstate
+FROM customers
+
+--prep
+
+--p: three columns, and a table
+--r: all rows from 3 columns 
+--e: columns honda,toyota,ford from table cars
+--p: Select query state of all three colums from the customers table
