f8a77c8eda137c8d8676e6b07b6124af19603d61
Roger David Cruz <roger.david.cruz@gmail.com>
2020-12-30 13:22:01 -0500

    my_head_is_at_the_wrong_end

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_my_head_is_at_the_wrong_end.js
@@ -0,0 +1,11 @@
+// 8 kyu - My head is at the wrong end!
+// https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/javascript
+
+const fixTheMeerkat = arr => arr.reverse();
+
+//prep
+
+//p: array of strings
+//r: array of reordered strings
+//e: ["tail", "body", "head"] => ["head", "body", "tail"]
+//p: use the reverse method on the parameter array
