64397f5a0c31f5c4d9da3f67c60e36a4d974602c
Roger David Cruz <roger.david.cruz@gmail.com>
2021-01-27 06:44:40 -0500

    volume_of_a_cuboid

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_volume_of_a_cuboid.js
@@ -0,0 +1,15 @@
+// 8 kyu - Volume of a Cuboid
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+//prep
+
+//p: three integers
+//r: one integer
+//e: p: 6.3,2,5 r: 63
+//p: multiply all three numbers and return that number.
