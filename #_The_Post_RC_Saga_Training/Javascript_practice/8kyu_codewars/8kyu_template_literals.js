e6c50a3eae02ddc87e853c84144a6db987dad29e
Roger David Cruz <roger.david.cruz@gmail.com>
2021-04-14 22:22:23 -0400

    codewar practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_template_literals.js
@@ -0,0 +1,6 @@
+// 8kyu - Template Strings
+// https://www.codewars.com/kata/55a14f75ceda999ced000048/train/javascript
+
+var TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`
+}
