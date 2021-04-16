30f636d519b3034c6a8a3a11470357caf3d2d52b
Roger David Cruz <roger.david.cruz@gmail.com>
2021-04-16 12:06:45 -0400

    codewars practice 7kyu

--- #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_three_sum_of_numbers.js
+++ #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_three_sum_of_numbers.js
@@ -1,6 +1,8 @@
 // 7 kyu - Beginner Series #3 Sum of Numbers
 // https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
 
+//Approach #1
+
 function getSum(a,b){
   if(a === b) return a;
     let nums = []
@@ -28,3 +30,34 @@ function getSum(a,b){
 //push into an empty array inside the loop
 //then if the array is not empty use reduce method to add all indexes of array
 //return the sum of numbers from the array
+
+//Approach #2
+
+getSum = (a,b) => {
+  let count = 0;
+ if(a === b){
+   return a;
+ }
+  if(b > a){
+    for(let i = a; i<=b; i++){
+      count = count + i
+    }
+    return count;
+  }
+  else{
+    for(let i = b; i<=a; i++){
+      count = count + i;
+    }
+    return count;
+  }
+}
+
+//prep
+
+//parameter: a and b are two integers (negative or positive) 
+//return: return 1 number and it's sum of all the numbers inbetween a and b
+//example: GetSum(-2,3) == 3 // -2 + -1 + 0 + 1 + 2 + 3 = 3
+//pseudo:  have a conditional if a === b then return a,
+//         order a and b with (simple check if a > b or b < a)
+//         populate the numbers in between (for loop)
+//         return the sum
