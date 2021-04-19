d63708c32fd0e75d95bd9c47fdb9c9cc50e4b287
Roger David Cruz <roger.david.cruz@gmail.com>
2021-04-18 23:19:48 -0400

    codewar 8kyu practice

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_take_the_first_N_elements.js
@@ -0,0 +1,11 @@
+// 8kyu - Enumerable Magic #25 - Take the First N Elements
+// https://www.codewars.com/kata/545afd0761aa4c3055001386/train/javascript
+
+take = (arr, n) => arr.slice(0,n);
+
+//prep
+
+//p: array and number
+//r: an array of the first n elements
+//e: [0, 1, 2, 3, 5, 8, 13], 3 => [0, 1, 2]
+//p: use the slice method to indicate where to start and cut off the array.
