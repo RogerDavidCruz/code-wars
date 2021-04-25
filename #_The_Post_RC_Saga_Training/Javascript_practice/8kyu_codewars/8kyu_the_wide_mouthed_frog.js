0fba5221b4bf99e9fad071e26190fb4207ba7069
Roger David Cruz <roger.david.cruz@gmail.com>
2021-04-25 15:24:47 -0400

    codewars practice - 8kyu

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_the_wide_mouthed_frog.js
@@ -0,0 +1,12 @@
+// 8 kyu - The Wide-Mouthed frog!
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/javascript  
+
+mouthSize = (animal) => animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+
+
+//prep
+
+//p: a string word
+//r: a string either small or wide
+//e: param: cat => wide, or alligator => small
+//p: use an else if condition to return two different values based on the param.
