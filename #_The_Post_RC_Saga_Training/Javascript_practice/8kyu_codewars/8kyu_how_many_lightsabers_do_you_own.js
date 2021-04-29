25b720dbb3c8155c5fcbcadc0a398283176969bf
Roger David Cruz <roger.david.cruz@gmail.com>
2021-04-29 12:35:32 -0400

    codewar practice 8kyu

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_how_many_lightsabers_do_you_own.js
@@ -0,0 +1,12 @@
+// 8 kyu - How many lightsabers do you own?
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/javascript
+
+howManyLightsabersDoYouOwn = (name) => name === "Zach" ? 18 : 0
+
+//PREP
+
+//P: name is a string
+//R: return either 18 or 0
+//E: "Zach" => 18 , "Roger" => 0
+//P: use a ternary statement in which checks is the parameter passed is "Zach", 
+//   if it is return 18, else return 0
