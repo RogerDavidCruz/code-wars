b4fd674c4d1c7e3175c925c6e3454158ac43b6f2
Roger David Cruz <roger.david.cruz@gmail.com>
2021-05-01 09:57:06 -0400

    codewar practice 8kyu

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_keep_up_the_hoop.js
@@ -0,0 +1,12 @@
+// 8 kyu - Keep up the hoop
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+
+hoopCount = (n) => n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+
+//PREP
+
+//P: a number
+//R: a string
+//E: 11 => "Great, now move on to tricks" 
+//P: a ternary statement for two different outputs based on the parameter
+//   value passed is greater than or equal to 10
