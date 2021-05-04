a6a150046c3f75cbead06ca80b5cf750345e0dc1
Roger David Cruz <roger.david.cruz@gmail.com>
2021-05-04 13:57:21 -0400

    codewar practice 7kyu

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_sort_arrays_1.js
@@ -0,0 +1,14 @@
+// 7 kyu - Sort arrays - 1
+// https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/javascript
+
+// input: names - unsorted array
+// output: sorted array
+sortme = (names) => names.sort()
+
+//PREP
+
+//P: array of string words
+//R: an array that is ordered alphabetically 
+//E: ['one', 'two', 'three' ] => ["one", "three", "two"]
+//P: use the sort method which orders the element that are strings inside an array
+//   in alphabetical order.
