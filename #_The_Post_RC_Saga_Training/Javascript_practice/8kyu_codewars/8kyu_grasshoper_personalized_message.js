3b9ba1d69e4dc9e01e0f94c97a8e620181238a05
Roger David Cruz <roger.david.cruz@gmail.com>
2021-05-05 11:27:58 -0400

    codewars practice 8kyu

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_grasshoper_personalized_message.js
@@ -0,0 +1,12 @@
+// 8 kyu - Grasshopper - Personalized Message
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/javascript
+
+greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest';
+
+//PREP
+
+//p: two strings
+//r: one string response 
+//e: 'Daniel', 'Daniel' => 'Hello boss'
+//p: use a ternary statement two output 'hello boss' if both parameters are the same
+//   otherwise output 'hello guest'
