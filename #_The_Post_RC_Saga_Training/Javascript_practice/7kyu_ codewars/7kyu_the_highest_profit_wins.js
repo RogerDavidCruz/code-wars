068eb0d8f3c1881336fc2c4089399e87b9734ff6
Roger David Cruz <roger.david.cruz@gmail.com>
2021-05-21 09:27:13 -0400

    codewars practice 7kyu

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/7kyu_ codewars/7kyu_the_highest_profit_wins.js
@@ -0,0 +1,14 @@
+// 7 kyu - The highest profit wins!
+// https://www.codewars.com/kata/559590633066759614000063/train/javascript  
+
+minMax = (arr) => [Math.min(...arr),Math.max(...arr)]
+
+//PREP
+
+//P: an array of numbers
+//R: an array of two numbers
+//E: [1,2,3,4,5] ==> [1,5]
+//P: Use math max and min methods to return the numbers
+//   we want and place them into an array to be returned
+//   Also use the array spread operator to check all the
+//   elements inside the array.
