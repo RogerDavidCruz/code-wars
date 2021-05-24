45f0a967e51bd2e2bcce7421d19d3d8339bf32f3
Roger David Cruz <roger.david.cruz@gmail.com>
2021-05-23 22:39:56 -0400

    codewars practice 8kyu

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_5_without_numbers.js
@@ -0,0 +1,15 @@
+// 8 kyu - 5 without numbers !!
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+function unusualFive() {
+    let array = ['','','','','']
+    return array.length
+  }
+
+//PREP
+
+//P: no argument passed into the function
+//R: the number 5 is always returned
+//E: ['','','','',''].length => 5
+//P: create an array of empty strings with a length of 5 elements
+//   then return the length of the array to always return 5
