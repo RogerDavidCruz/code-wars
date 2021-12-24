1a8efa222749402c029c1c2324023b95c94c790b
Roger David Cruz <roger.david.cruz@gmail.com>
2021-12-23 19:34:55 -0500

    parse_nice_int_from_char_problem

---
+++ #_The_Post_RC_Saga_Training/Javascript_practice/8kyu_codewars/8kyu_parse_nice_int_from_char_problem.js
@@ -0,0 +1,9 @@
+// One Solution
+
+const getAge = inputString => Number(inputString[0]);
+
+// Second Solution
+
+const getAge = inputString => parseInt(inputString);
+
+//Note: Using parseInt on string sentence will only return the numerical value of a string number.
